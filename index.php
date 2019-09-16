c46f7c286a9d9afee690eb36af2270349d89c502
Baron <arthur.baron@epitech.eu>
2019-09-16 17:49:43 +0200

    16/09|17:48|master| Fifth commit, tp jour 2 jusque exercice 5

--- index.php
+++ index.php
@@ -8,19 +8,19 @@
 
 <body>
     <header>
-    <?php include '/var/www/html/inc/navbar.html' ?>
+        <?php include '/var/www/html/inc/navbar.html' ?>
         <div class="jumbotron">
             <h1 class="lead">Welcome to coding Pedia</h1>
         </div>
         <!-- That will became a php include to put it on every pages -->
         <p>Cliquer sur le menu</p>
         <menu type="toolbar" id="popup-menu" label="go to">
-                <menuitem label="Home" onclick="">
-                </menuitem>
-                <menuitem label="PHP" onclick="">
-                </menuitem>
-                <menuitem label="SQL" onclick="">
-                </menuitem>
+            <menuitem label="Home" onclick="">
+            </menuitem>
+            <menuitem label="PHP" onclick="">
+            </menuitem>
+            <menuitem label="SQL" onclick="">
+            </menuitem>
         </menu>
     </header>
 
@@ -28,15 +28,15 @@
     <main>
         <div class="container">
             <div class="row">
-                <div class="col-md-6">
-                    <p>
+                <div class="col-md-6 bg-success">
+                    <p class="first-paragraph left-bordered-and-big">
                         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam suscipit quibusdam quisquam maxime asperiores temporibus magnam labore pariatur delectus, ut, excepturi vero quo omnis nulla ducimus enim accusamus nobis, dolore unde incidunt deleniti eaque
                         eveniet! Ratione, illum impedit architecto nemo autem quidem necessitatibus officia, laboriosam est, porro dolores recusandae libero.
                     </p>
                 </div>
                 <div class="col-md-6">
                     <aside>
-                        <p>
+                        <p class="second-paragraph right-bordered-and-small">
                             My name is Arthur, I've been studying IT for four years....
                         </p>
                     </aside>
@@ -80,6 +80,21 @@
                             Submit
                         </button>
                     </form>
+                </div>
+            </div>
+            <div class="row mt-5">
+                <div class="col-md-12">
+                    <h2>Snow white story (short version)</h2>
+                    <hr>
+                </div>
+            </div>
+            <div class="row mt-3 mb-5">
+                <div class="col-md-6">
+                    <p class="a-great-style">Once upon a time there lived a lovely princess with fair skin and blue eyes . She was
+                        so fair that she was named Snow White .</p>
+                </div>
+                <div class="col-md-6">
+                    <p class="a-great-style">One day , she was knitting and pricked herself . Three drops of <span class="blood">blood</span> fell in the snow .</p>
                 </div>
             </div>
         </div>
